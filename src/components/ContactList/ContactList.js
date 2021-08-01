1f8bf576eab80984350e05c6af8afaa0a00532f5
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-08-01 19:40:06 +0200

    Clear code

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,86 +1,37 @@
-// import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ContactList.module.css';
 
 
 function ContactList({ contacts, onDeleteContact }) {
-    return (
-      <ul className={styles.list}>
-        {contacts.map(({ id, name, number }) => (
-          <li key={id} className={styles.item}>
-            <p className={styles.text}>
-              {name}: {number}
-            </p>
-            <button
-              type="button"
-              onClick={() => onDeleteContact(id)}
-              className={styles.button}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul className={styles.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id} className={styles.item}>
+          <p className={styles.text}>
+            {name}: {number}
+          </p>
+          <button
+            type="button"
+            onClick={() => onDeleteContact(id)}
+            className={styles.button}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  onDeleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+};
   
-  ContactList.propTypes = {
-    onDeleteContact: PropTypes.func.isRequired,
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      }),
-    ),
-  };
-  
-  export default ContactList;
-
-
-
-// const ContactListItem = ({name, number, onClickRemove}) => {
-//     return (
-//         <li className={styles.contactListItem}>
-//             <p>{name}: {number}</p>
-//             <button 
-//                 type="button" 
-//                 className={styles.contactListButton} 
-//                 onClick={onClickRemove}
-//             >
-//                 Delete
-//             </button>
-//         </li>
-//     )
-// }
-
-// const ContactList = ({filteredContacts, onRemove}) => {
-//     return (
-//         filteredContacts.length > 0 && (
-//             <ul className={styles.contactList}>
-//                 {filteredContacts.map(({id, name, number}) => (
-//                     <ContactListItem key={id} name={name} number={number} onClickRemove={() => onRemove(id)} />
-//                 ))}
-//             </ul>
-//         )
-//     )
-// }
-
-// ContactListItem.propTypes = {
-//     name: PropTypes.string.isRequired,
-//     number: PropTypes.string.isRequired,
-//     onClickRemove: PropTypes.func.isRequired,
-// }
-
-// ContactList.propTypes = {
-//     filteredContacts: PropTypes.arrayOf(
-//         PropTypes.exact({
-//             id: PropTypes.string.isRequired,
-//             name: PropTypes.string.isRequired,
-//             number: PropTypes.string.isRequired,
-//         })
-//         ),
-//         onRemove: PropTypes.func.isRequired,
-// }
-
-// export default ContactList;
+export default ContactList;
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,18 +3,18 @@ import styles from './Filter.module.css';
 
 
 function Filter({ value, onChange }) {
-    return (
-      <label>
-        Find contacts by name:
-        <input
-          className={styles.input}
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </label>
-    );
-  }
+  return (
+    <label>
+      Find contacts by name:
+      <input
+        className={styles.input}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
   
 Filter.propTypes = {
     value: PropTypes.string.isRequired,
